db111b5355e66184c920b746bc27090d733a00b4
alikulovuzz <azamat.aliqulov@list.ru>
2024-01-19 18:15:38 +0500

    Unical Tasks

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import muiTheme from "./theme/muiTheme";
+import defaultTheme from "./theme/defaultTheme";
+import { ThemeProvider } from 'styled-components'
+import { ThemeProvider as MuiTheme } from '@mui/material/styles'
+import { BrowserRouter, Routes, Route, Navigate, useNavigate, Router } from "react-router-dom"
+import SignInSide from "./components/SignInSide";
+import { useSelector } from "react-redux";
+import Main from "./components/Main";
+import PagePost from "./components/PagePost";
+import ErrorPage from "./components/404";
+import PageTodos from "./components/PageTodos";
+import PageUsers from "./components/PageUser";
+import PageTodoUser from "./components/PageTodoUser";
+import { Outlet } from 'react-router-dom'
+
+
+function App() {
+
+  const user = useSelector((state) => state.user);
+
+  return (
+    <MuiTheme theme={muiTheme}>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<SignInSide />} errorElement={<ErrorPage />} />
+            {sessionStorage.getItem("access_token") || user ? (<>
+              <Route path="/" element={<Main />} errorElement={<ErrorPage />}>
+                <Route index element={<PagePost />} />
+                <Route path="todo" element={<PageTodos />}>
+                  <Route path=":id" element={<>todo by id</>} />
+                </Route>
+                <Route path="users" element={<Outlet />}>
+                  <Route index element={<PageUsers />} />
+                  <Route path=":id" element={<PageTodoUser />} />
+                </Route>
+              </Route>
+            </>) : (<></>)}
+            <Route path="*" element={<Navigate to={"/login"} errorElement={<ErrorPage />} />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </MuiTheme>
+  );
+}
+
+export default App;
